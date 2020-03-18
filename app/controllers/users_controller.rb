1056267bbef2c42a1b9281fb3ca98bb3462557f8
bafiam <stevebafiam@gmail.com>
2020-03-18 20:13:23 +0300

    add user controller, and signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      session[:id] = @user.id 
+      redirect_to @user
+    else
+      render :new
+    end
+
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
+end
