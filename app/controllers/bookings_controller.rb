f4b6e3e767bfd4778580c16b73d1d43786c92945
simoxsimo <touili.mohammed@gmail.com>
2020-03-19 22:43:44 +0000

    Added Invitation part (Not completed)

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,22 @@
+class BookingsController < ApplicationController
+  def new
+    @invite = Booking.new
+  end
+
+  def create
+    @invite = Booking.create(invite_params)
+    if @invite.save
+      flash.now[:success] = 'Your Invatation has been successfully sent'
+      render :new
+    else
+      flash.now[:danger] = 'Error!! You invitation isn\'t sent'
+      render :new
+    end
+  end
+
+  private
+
+  def invite_params
+    params.require(:booking).permit(:attendee_id, :attended_event_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'bookings/new'
+  get 'bookings/create'
   root to: 'users#new'
   get '/signup', to: 'users#new'
-  post'/signup',  to: 'users#create'
+  post '/signup', to: 'users#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
   resources :events, only: [:new, :create, :show, :index]
+  resources :bookings, only: [:new, :create]
 end
