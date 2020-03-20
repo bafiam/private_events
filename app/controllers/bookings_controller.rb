29b950e1bc14da0db362db625403db1de36a970b
simoxsimo <touili.mohammed@gmail.com>
2020-03-20 15:51:39 +0000

    Finished Inviting users to events

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,19 +4,26 @@ class BookingsController < ApplicationController
   end
 
   def create
-    @invite = Booking.create(invite_params)
-    if @invite.save
+    @invite = Booking.new(invite_params)
+    if !query_check
+      @invite.save
       flash.now[:success] = 'Your Invatation has been successfully sent'
-      redirect_to event_path(current_user) 
+    elsif @invite.attended_event_id == query_check.attended_event_id && \
+          @invite.attendee_id == query_check.attendee_id
+      flash.now[:warning] = 'Error!! User already invited!!'
     else
-      flash.now[:danger] = 'Error!! You invitation isn\'t sent'
-      render :new
+      flash.now[:danger] = 'Error!! Can\'t invite user'
     end
+    render :new
   end
 
   private
 
   def invite_params
     params.require(:booking).permit(:attendee_id, :attended_event_id)
+  end
+
+  def query_check
+    Booking.find_by(invite_params)
   end
 end
