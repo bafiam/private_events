373992571d8c4c60be172ba12a0c093910331926
bafiam <stevebafiam@gmail.com>
2020-03-20 17:40:02 +0300

    add styling and links for presentation

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,7 +7,7 @@ class BookingsController < ApplicationController
     @invite = Booking.create(invite_params)
     if @invite.save
       flash.now[:success] = 'Your Invatation has been successfully sent'
-      render :new
+      redirect_to event_path(current_user) 
     else
       flash.now[:danger] = 'Error!! You invitation isn\'t sent'
       render :new
